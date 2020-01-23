8be8a649f162d452c034a7d859c7c669f81a3e12
arditsarja <a.sarja@evolutivo.it>
2020-01-23 12:16:33 +0100

    bat files

---
+++ corebos/info.bat
@@ -0,0 +1,4 @@
+echo The current directory is %CD%
+cd %CD%
+java -jar corebos-1.0-SNAPSHOT-jar-with-dependencies.jar
+pause >nul
---
+++ corebos/producer.bat
@@ -0,0 +1,4 @@
+echo The current directory is %CD%
+cd %CD%
+java -cp corebos-1.0-SNAPSHOT-jar-with-dependencies.jar SyncProducerExe
+pause >nul
