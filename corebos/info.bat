4319ed9b25bba61719411bde41b8908f3e591729
arditsarja <a.sarja@evolutivo.it>
2020-01-30 18:07:41 +0100

    adding deploy folder

--- corebos/info.bat
+++
@@ -1,4 +0,0 @@
-echo The current directory is %CD%
-cd %CD%
-java -jar corebos-1.0-SNAPSHOT-jar-with-dependencies.jar
-pause >nul
--- corebos/producer.bat
+++
@@ -1,4 +0,0 @@
-echo The current directory is %CD%
-cd %CD%
-java -cp corebos-1.0-SNAPSHOT-jar-with-dependencies.jar SyncProducerExe
-pause >nul
