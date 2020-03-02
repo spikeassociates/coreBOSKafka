efe846fa9078be374deaefc9b10c319aa20af41c
arditsarja <a.sarja@evolutivo.it>
2020-03-02 15:52:05 +0100

    removing unnecessary files

--- src/main/java/stream/Stream.java
+++
@@ -1,9 +0,0 @@
-package stream;
-
-import helper.Util;
-
-public class Stream {
-    public static final String firstStreamTopic ="first_stream_topic";
-    public static final String secondStreamTopic ="second_stream_topic";
-    public static final String KAFKA_URL = Util.getProperty("corebos.kafka.url");
-}
