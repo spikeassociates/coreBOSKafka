f25d9dd67d2bf0bc582b91425f0367b50e21860a
arditsarja <a.sarja@evolutivo.it>
2020-04-01 17:31:44 +0200

    undo check logomanifest base 64 for manifestazioni

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 import helper.Util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Map map = new HashMap();
+        map.put("Ss", "sdf");
+
+        if (map.get("S") != null) {
+            String s = map.get("S").toString().replace("s", "a");
+            System.out.println("s = " + s);
+        }
         for (String arg : args)
             System.out.println("arg = " + arg);
         System.out.println("Hello World");
