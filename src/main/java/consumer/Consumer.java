5eb4f4509029aa26f41fa85ce7321d06ad43aba4
Mario Anthony <anthonyjeremiah1956@gmail.com>
2020-05-15 18:43:12 +0300

    Update Value for consumer session.timeout.ms value

--- src/main/java/consumer/Consumer.java
+++ src/main/java/consumer/Consumer.java
@@ -35,12 +35,12 @@ public class Consumer {
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put("group.id", GROUP_ID);
 //        properties.put("max.partition.fetch.bytes", 2097152);
-        properties.put("max.poll.records", 200);
+        properties.put("max.poll.records", 1000);
 //        properties.put("auto.commit.interval.ms", 8000);
         properties.put("enable.auto.commit", true);
 //        properties.put("request.timeout.ms", 305000);
         properties.put("max.poll.interval.ms", 500000);
-//        properties.put("session_timeout_ms", 180000);
+        properties.put("session.timeout.ms", 500000);
 //        properties.put("heartbeat_interval_ms", 60000);
 //        properties.put("metadata_max_age_ms", 50000);
         kafkaConsumer = new KafkaConsumer(properties);
